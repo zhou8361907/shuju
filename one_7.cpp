40ef4e403150d0262e65bfe9c687951797448e7f
zhou8361907 <947452262@qq.com>
2017-08-04 11:52:17 +0800

    数位之和
    将一个长数字的每一位取出并相加，得出得数。
    要注意使用取余号%。

---
+++ one_7.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//using namespace std;
+//
+////依次取一个数的每一位时，记得用%取余
+//int main(){
+//	long n;
+//	cin >> n;
+//	int i = 0;
+//
+//	while (n){
+//		i += n % 10;
+//		n = n / 10;
+//		
+//	}
+//	cout << i;
+//}
