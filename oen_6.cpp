d5297924d354408fbea4ed1e121df4023f28caff
zhou8361907 <947452262@qq.com>
2017-08-04 11:50:30 +0800

    数列分段
    将一组数分割，若相同则分为一段，统计段数

---
+++ oen_6.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main()
+//{
+//
+//	int n;
+//	cin >> n;
+//	int *a = new int[n];
+//	for (int i = 0; i < n; i++){
+//		cin >> a[i];
+//
+//	}
+//	int t =0;
+//	for (int i = 0; i < n; i++){
+//		if (a[i] != a[i + 1])
+//			t++;
+//	
+//	}
+//	cout << t;
+//}
